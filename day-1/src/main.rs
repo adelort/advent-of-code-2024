af3937e71ffb5c1c16a7f8811387e5b580581a51
Arthur <arthurdelort@hotmail.fr>
2024-12-28 10:09:20 +0100

    Day 1 - Part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 const FILE_PATH: &str = "src/pairs.txt";
 
@@ -24,4 +24,30 @@ fn main() {
         .sum();
 
     println!("The sum is {sum}");
+
+    let mut similarity_score = 0;
+
+    let key_count: HashMap<i32, i32> = contents.lines().fold(HashMap::new(), |mut acc, line| {
+        let x = line
+            .split_whitespace()
+            .nth(1)
+            .and_then(|item| item.parse::<i32>().ok())
+            .unwrap_or(0);
+
+        *acc.entry(x).or_insert(0) += 1;
+        acc
+    });
+
+    contents.lines().for_each(|line| {
+        let parsed_first = line
+            .split_whitespace()
+            .next()
+            .and_then(|x| x.parse::<i32>().ok());
+
+        if let Some(parsed) = parsed_first {
+            similarity_score += parsed * *key_count.get(&parsed).unwrap_or(&0);
+        }
+    });
+
+    println!("The similarity score is {similarity_score}");
 }
